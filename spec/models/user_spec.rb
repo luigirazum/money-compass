81d8c6a2001781a1cb9c4376e4890a0badf863ea
Luis Zubia <soyzubia@gmail.com>
2024-02-28 02:55:12 -0700

    Add 'unit test' for 'User' model: is valid with valid attributes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
+# 'attr_mod' will allow you to test 'obj'
+# with the 'mod' attribute passed.
+def attr_mod(mod, obj: user)
+  obj[mod.keys.first] = mod.values.first
+  obj
+end
+
 RSpec.describe User, type: :model do
   let!(:user) do
     described_class.new(name: 'user name')
@@ -10,6 +17,9 @@ RSpec.describe User, type: :model do
   describe ".new 'User' is valid only:", :valid do
     it '- with valid attributes' do
       expect(user).to be_valid
+      expect(attr_mod({ name: nil })).to_not be_valid
+      expect(attr_mod({ name: '' })).to_not be_valid
+      expect(attr_mod({ name: '    ' })).to_not be_valid
     end
   end
 end
