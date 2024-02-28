c6b9be021efebaccd88caf9e50b5b97c8f1ef64c
Luis Zubia <soyzubia@gmail.com>
2024-02-28 03:42:01 -0700

    Add 'Category' model validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,8 @@
 class Category < ApplicationRecord
+  # validations
+  validates_presence_of :name
+  validates_length_of :name, within: 5..35
+  validates_uniqueness_of :name
+  validates_presence_of :icon
+  validates_format_of :icon, with: %r{\Ahttps?://.*\.?(jpg|jpeg|png|gif|webp|avif|svg)?\z}i
 end
