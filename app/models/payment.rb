abf9901bf0acfafb0407e77abeb79c42785bf571
Luis Zubia <soyzubia@gmail.com>
2024-02-28 04:22:04 -0700

    Add 'Payment' model validations

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,2 +1,7 @@
 class Payment < ApplicationRecord
+  # validations
+  validates_presence_of :name
+  validates_length_of :name, within: 5..150
+  validates_presence_of :amount
+  validates_numericality_of :amount, greater_than: 0
 end
