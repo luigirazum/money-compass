11806018e96c888ead1560223ddb9f05f4793a39
Luis Zubia <soyzubia@gmail.com>
2024-03-01 04:42:57 -0700

    Create 'render_content_for' to render conditional content at the application layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def render_content_for
+    content_for :header do
+      render 'nav_header'
+    end
+
+    content_for :messages do
+      render 'flash_msgs'
+    end
+  end
 end
