6ece762f447a8a3edb46362fb2c4aa990952384f
Luis Zubia <soyzubia@gmail.com>
2024-03-01 01:18:21 -0700

    Configure 'devise' for 'User' model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  # Include devise modules. Others available are:
+  # :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable
+
   # associations
   has_many :categories, inverse_of: :user
   has_many :payments, inverse_of: :author, foreign_key: :author_id
