ff35c59e8efe8b9d2e7a51b06c4b7cc6bf25439f
Luis Zubia <soyzubia@gmail.com>
2024-03-01 03:59:22 -0700

    Create seeds to have content for render

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,31 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+def icon_link(icon)
+  "https://fakeimg.pl/160x160/252f3f,255/f29800,255/?font=roboto&text=#{icon}"
+end
+
+def user_categories(uname)
+  (0..10).to_a.sample(rand(uname.size) + rand(5)).sample
+end
+
+def cat_payments(cat_count)
+  (0..10).to_a.sample(rand(cat_count) + rand(5)).sample
+end
+
+%w[Kevin Marco Kyle].each do |uname|
+  User.create!(name: uname, email: "#{uname}@me.com", password: 'pwd1234', confirmed_at: Time.now())
+end
+
+User.all.each do |u|
+  total_categories = user_categories(u.name)
+  next unless total_categories
+  total_categories.times do |n|
+    cat = u.categories.create!(name: "Category ##{n + 1}", icon: icon_link("Cat+#{n + 1}"))
+    total_payments = cat_payments(n + 1)
+    next unless total_payments
+    total_payments.times do |t|
+      cat.payments.create!(name: "Payment ##{t + 1} for Category ##{n + 1}", amount: 1.0)
+    end
+  end
+end
