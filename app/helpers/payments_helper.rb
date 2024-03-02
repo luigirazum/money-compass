2309f91d6d4781422154425a03dea69766276792
Luis Zubia <soyzubia@gmail.com>
2024-03-02 01:14:39 -0700

    Add helper to render payments

--- app/helpers/payments_helper.rb
+++ app/helpers/payments_helper.rb
@@ -1,2 +1,9 @@
 module PaymentsHelper
+  def render_payments_by_category
+    if @payments.size.positive?
+      render partial: 'payment', layout: 'list_item', collection: @payments
+    else
+      render 'application/empty_collection', subject: 'category', target: 'payments'
+    end
+  end
 end
